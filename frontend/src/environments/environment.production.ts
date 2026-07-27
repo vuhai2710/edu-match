8986ee19f4c97136e16479fde9c92f5a06a52b18
vuhai <vuhai271004@gmail.com>
2026-07-27 23:40:40 +0700

    deploy fe

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -3,9 +3,9 @@ import { AppEnv } from '../app/core/config/app-env';
 export const environment: AppEnv = {
   production: true,
   appName: 'EduMatch',
-  appBaseUrl: 'https://www.edumatch.online',
-  apiBaseUrl: 'https://api.edumatch.online',
-  hubBaseUrl: 'https://api.edumatch.online',
+  appBaseUrl: 'https://proud-pond-0eab79500.7.azurestaticapps.net',
+  apiBaseUrl: 'https://edumatch-api-gdagajg2gue9atb0.southeastasia-01.azurewebsites.net',
+  hubBaseUrl: 'https://edumatch-api-gdagajg2gue9atb0.southeastasia-01.azurewebsites.net',
   googleClientId: '156523713376-8gk7ckvh0332kjgcpbb2qjvq2o4br68i.apps.googleusercontent.com',
   paymentReturnRoutes: {
     success: '/payment/success',
